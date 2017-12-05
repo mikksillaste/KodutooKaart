c9b051408fc91e5b990fe2be216e11921ad2ca60
mikksillaste <mikk.sillaste@gmail.com>
2017-12-05 22:46:34 +0200

    change

--- Kaart/urls.py
+++ Kaart/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^api/data$', views.get_kohad, name='api-data'),
+    url(r'^api/kohad$', views.add_koht, name='kohad'),
 ]
--- Kaart/views.py
+++ Kaart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 
 from Kaart import models
+from Kaart.models import Kohad
 
 
 def index(request):
@@ -11,3 +12,14 @@ def index(request):
 def get_kohad(request):
     kohad = models.Kohad.objects.all().values('text', 'lat', 'lng')
     return JsonResponse(list(kohad), safe=False)
+
+
+def add_koht(request):
+    if request.is_ajax():
+        if request.method == 'GET':
+            kohad = Kohad()
+            kohad.text = request.GET.get('text')
+            kohad.lat = request.GET.get('lat')
+            kohad.lng = request.GET.get('lng')
+            kohad.save()
+            return render(request, 'index.html')
